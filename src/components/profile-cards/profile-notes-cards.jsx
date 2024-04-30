3ad81cbc811fc259446b61b961b735caf9de388b
Sam Page <sampage4427@gmail.com>
2024-04-30 17:31:54 -0600

    style: updated style for the show more cards and the gap between cards

--- src/components/profile-cards/profile-notes-cards.jsx
+++ src/components/profile-cards/profile-notes-cards.jsx
@@ -3,7 +3,7 @@ import { dummyItems } from "../../utils/constants";
 
 export const ProfileNotesCards = () => {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-3 gap-5">
       {/* Will need to sort by most recent date and go backward from that */}
       {dummyItems.length <= 3
         ? dummyItems.map((item) => (
@@ -29,7 +29,7 @@ export const ProfileNotesCards = () => {
             // opens modal for note preview or notes page is show more card is clicked
           }}
         >
-          <h3 className="text-xl font-minaBold text-zinc-800 mb-2.5">
+          <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
             + {dummyItems.length - 2} More
           </h3>
         </div>
