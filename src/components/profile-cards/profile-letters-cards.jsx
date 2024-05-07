819060e57e48030d7818e44f3a486cec097439bd
Sam Page <sampage4427@gmail.com>
2024-05-06 20:45:28 -0600

    fix: empty notes and letters if there is no data to display

--- src/components/profile-cards/profile-letters-cards.jsx
+++ src/components/profile-cards/profile-letters-cards.jsx
@@ -2,6 +2,7 @@ import { ProfileLetterCard } from "./profile-letter-card";
 import { dummyItems } from "../../utils/constants";
 import { useMediaQuery } from "react-responsive";
 import { useNavigate } from "react-router-dom";
+import { SentimentVeryDissatisfied } from "@mui/icons-material";
 
 export const ProfileLettersCards = ({ children }) => {
   return <>{children}</>;
@@ -15,63 +16,74 @@ ProfileLettersCards.Preview = () => {
   const router = useNavigate();
 
   return (
-    <div className={`grid gap-5 ${xlScreen ? "grid-cols-6" : "grid-cols-3"}`}>
-      {/* Will need to sort by most recent date and go backward from that */}
-      {dummyItems.length <= 3
-        ? dummyItems.map((item) => (
-            <ProfileLetterCard
-              key={item.title}
-              title={item.title}
-              content={item.content}
-            />
-          ))
-        : xlScreen
-          ? dummyItems
-              .slice(0, 5)
-              .map((item) => (
+    <>
+      {dummyItems.length > 0 ? (
+        <div
+          className={`grid gap-5 ${xlScreen ? "grid-cols-6" : "grid-cols-3"}`}
+        >
+          {/* Will need to sort by most recent date and go backward from that */}
+          {dummyItems.length <= 3
+            ? dummyItems.map((item) => (
                 <ProfileLetterCard
                   key={item.title}
                   title={item.title}
                   content={item.content}
                 />
               ))
-          : dummyItems
-              .slice(0, 2)
-              .map((item) => (
-                <ProfileLetterCard
-                  key={item.title}
-                  title={item.title}
-                  content={item.content}
-                />
-              ))}
-      {dummyItems.length > 3 && !xlScreen ? (
-        <div
-          className="flex flex-col justify-center border border-black shadow-md rounded-lg p-6 h-[400px] transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl text-center"
-          onClick={() => {
-            // opens letters page
-            router("/profile/letters");
-          }}
-        >
-          <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
-            + {dummyItems.length - 2} More
-          </h3>
-        </div>
-      ) : dummyItems.length > 5 && xlScreen ? (
-        <div
-          className="flex flex-col justify-center border border-black shadow-md rounded-lg p-6 h-[400px] transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl text-center"
-          onClick={() => {
-            // opens letters page
-            router("/profile/letters");
-          }}
-        >
-          <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
-            + {dummyItems.length - 2} More
-          </h3>
+            : xlScreen
+              ? dummyItems
+                  .slice(0, 5)
+                  .map((item) => (
+                    <ProfileLetterCard
+                      key={item.title}
+                      title={item.title}
+                      content={item.content}
+                    />
+                  ))
+              : dummyItems
+                  .slice(0, 2)
+                  .map((item) => (
+                    <ProfileLetterCard
+                      key={item.title}
+                      title={item.title}
+                      content={item.content}
+                    />
+                  ))}
+          {dummyItems.length > 3 && !xlScreen ? (
+            <div
+              className="flex flex-col justify-center border border-black shadow-md rounded-lg p-6 h-[400px] transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl text-center"
+              onClick={() => {
+                // opens letters page
+                router("/profile/letters");
+              }}
+            >
+              <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
+                + {dummyItems.length - 2} More
+              </h3>
+            </div>
+          ) : dummyItems.length > 5 && xlScreen ? (
+            <div
+              className="flex flex-col justify-center border border-black shadow-md rounded-lg p-6 h-[400px] transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl text-center"
+              onClick={() => {
+                // opens letters page
+                router("/profile/letters");
+              }}
+            >
+              <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
+                + {dummyItems.length - 2} More
+              </h3>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       ) : (
-        ""
+        <h2 className="font-minaRegular text-4xl mb-7">
+          You have not written any letters yet{" "}
+          <SentimentVeryDissatisfied fontSize="large" />
+        </h2>
       )}
-    </div>
+    </>
   );
 };
 
@@ -94,8 +106,9 @@ ProfileLettersCards.MyLetters = () => {
           ))}
         </div>
       ) : (
-        <h2 className="font-minaBold text-4xl mb-7">
-          You have not written any letters
+        <h2 className="font-minaRegular text-4xl mb-7">
+          You have not written any letters{" "}
+          <SentimentVeryDissatisfied fontSize="large" />
         </h2>
         // button that leads to create-letters page
       )}
--- src/components/profile-cards/profile-notes-cards.jsx
+++ src/components/profile-cards/profile-notes-cards.jsx
@@ -2,6 +2,7 @@ import { ProfileNoteCard } from "./profile-note-card";
 import { dummyItems } from "../../utils/constants";
 import { useMediaQuery } from "react-responsive";
 import { useNavigate } from "react-router-dom";
+import { SentimentVeryDissatisfied } from "@mui/icons-material";
 
 export const ProfileNotesCards = ({ children }) => {
   // screen size dependent cards. Also should be centered on 4k screens
@@ -16,63 +17,74 @@ ProfileNotesCards.Preview = () => {
   const router = useNavigate();
 
   return (
-    <div className={`grid gap-5 ${xlScreen ? "grid-cols-6" : "grid-cols-3"}`}>
-      {/* Will need to sort by most recent date and go backward from that until limit of 3 or 5 depending on screensize reached */}
-      {dummyItems.length <= 3
-        ? dummyItems.map((item) => (
-            <ProfileNoteCard
-              key={item.title}
-              title={item.title}
-              content={item.content}
-            />
-          ))
-        : xlScreen
-          ? dummyItems
-              .slice(0, 5)
-              .map((item) => (
+    <>
+      {dummyItems > 0 ? (
+        <div
+          className={`grid gap-5 ${xlScreen ? "grid-cols-6" : "grid-cols-3"}`}
+        >
+          {/* Will need to sort by most recent date and go backward from that until limit of 3 or 5 depending on screensize reached */}
+          {dummyItems.length <= 3
+            ? dummyItems.map((item) => (
                 <ProfileNoteCard
                   key={item.title}
                   title={item.title}
                   content={item.content}
                 />
               ))
-          : dummyItems
-              .slice(0, 2)
-              .map((item) => (
-                <ProfileNoteCard
-                  key={item.title}
-                  title={item.title}
-                  content={item.content}
-                />
-              ))}
-      {dummyItems.length > 3 && !xlScreen ? (
-        <div
-          className="flex flex-col justify-center border border-black shadow-md rounded-lg p-6 h-[400px] transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl text-center"
-          onClick={() => {
-            // opens notes page
-            router("/profile/notes");
-          }}
-        >
-          <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
-            + {dummyItems.length - 2} More
-          </h3>
-        </div>
-      ) : dummyItems.length > 5 && xlScreen ? (
-        <div
-          className="flex flex-col justify-center border border-black shadow-md rounded-lg p-6 h-[400px] transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl text-center"
-          onClick={() => {
-            // opens notes page
-            router("/profile/notes");
-          }}
-        >
-          <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
-            + {dummyItems.length - 5} More
-          </h3>
+            : xlScreen
+              ? dummyItems
+                  .slice(0, 5)
+                  .map((item) => (
+                    <ProfileNoteCard
+                      key={item.title}
+                      title={item.title}
+                      content={item.content}
+                    />
+                  ))
+              : dummyItems
+                  .slice(0, 2)
+                  .map((item) => (
+                    <ProfileNoteCard
+                      key={item.title}
+                      title={item.title}
+                      content={item.content}
+                    />
+                  ))}
+          {dummyItems.length > 3 && !xlScreen ? (
+            <div
+              className="flex flex-col justify-center border border-black shadow-md rounded-lg p-6 h-[400px] transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl text-center"
+              onClick={() => {
+                // opens notes page
+                router("/profile/notes");
+              }}
+            >
+              <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
+                + {dummyItems.length - 2} More
+              </h3>
+            </div>
+          ) : dummyItems.length > 5 && xlScreen ? (
+            <div
+              className="flex flex-col justify-center border border-black shadow-md rounded-lg p-6 h-[400px] transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl text-center"
+              onClick={() => {
+                // opens notes page
+                router("/profile/notes");
+              }}
+            >
+              <h3 className="text-2xl font-minaBold text-zinc-800 mb-2.5">
+                + {dummyItems.length - 5} More
+              </h3>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       ) : (
-        ""
+        <h2 className="font-minaRegular text-4xl mb-7">
+          You do not have any notes yet{" "}
+          <SentimentVeryDissatisfied fontSize="large" />
+        </h2>
       )}
-    </div>
+    </>
   );
 };
 
@@ -97,8 +109,9 @@ ProfileNotesCards.MyNotes = () => {
           ))}
         </div>
       ) : (
-        <h2 className="font-minaBold text-4xl mb-7">
-          You do not have any notes
+        <h2 className="font-minaRegular text-4xl mb-7">
+          You do not have any notes{" "}
+          <SentimentVeryDissatisfied fontSize="large" />
         </h2>
         // button to open notes modal
       )}
